e796d852b27ba21cbb81fba72293c5185b1682cc
nzeesports <fakefake3179@gmail.com>
2025-02-06 10:56:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-$get = $_GET['get'];
-$mpdUrl = 'https://ucdn.starhubgo.com/' . $get;
-
-$mpdheads = [
-  'http' => [
-      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
-      'follow_location' => 1,
-      'timeout' => 5
-  ]
-];
-$context = stream_context_create($mpdheads);
-$res = file_get_contents($mpdUrl, false, $context);
-echo $res;
+$url = "https://ucdn.starhubgo.com/bpk-tv/HubSports5HD/output/manifest.mpd";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+if (!curl_errno($ch)) {
+    header("Content-Type: application/dash+xml");
+    echo $response;
+}
+curl_close($ch);
 ?>
